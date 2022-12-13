755f6595ee1f28ae3010e474b5b3ec344667f701
jdh050623 <jdh050623@gmail.com>
2022-12-13 23:08:22 +0900

    스네이크 게임 기본 틀

---
+++ Assets/CoinSpawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour
+{
+    public GameObject food_Prefab;
+
+    public Transform border_Bottom;
+    public Transform border_Top;
+    public Transform border_Lift;
+    public Transform border_Right;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
